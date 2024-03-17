7d8e75f229f330084b6442c460911612dcb8d2d2
Buket Aytac <buketnuraytac@gmail.com>
2024-03-17 23:15:48 +0300

    APPBAR
    appbar klasörünü eklemeyi unutmuşum sry

---
+++ yonder_app/lib/navigation/navigation_bar.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:yonder_app/pages/rota.dart';
+
+class NavigationBar extends StatefulWidget {
+  const NavigationBar({super.key});
+
+  @override
+  State<NavigationBar> createState() => _NavigationBarState();
+}
+//var pageList= [Rota(),Maker(),Gemini(),History(),Profile()]
+class _NavigationBarState extends State<NavigationBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
+
